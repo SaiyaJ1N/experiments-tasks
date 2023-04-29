6e644391310b44fbf6d41ee92f504930453d0072
Vitalii <mkukoba11@ukr.net>
2023-04-29 15:36:16 +0300

    Added Isogram.java

---
+++ src/Isogram.java
@@ -0,0 +1,23 @@
+public class Isogram {
+    public static void main(String[] args) {
+        boolean isIsogram = isIsogram("moOse");
+        System.out.println(isIsogram);
+    }
+
+    public static boolean  isIsogram(String str) {
+        if(str.length() == 0){
+            return true;
+        }
+            String newStr = "" + str.charAt(0);
+            for(int i = 1; i < str.length(); i++){
+                newStr += str.charAt(i);
+                for(int j = 0; j < i; j++){
+                    if(str.charAt(i) == str.charAt(j) || str.charAt(i) == Character.toUpperCase(str.charAt(j))){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+}
